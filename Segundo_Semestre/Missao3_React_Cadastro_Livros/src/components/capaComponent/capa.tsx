a409965dfb06d62df52084bccccb2e0033bcdd2f
jmoka <jmokatavares@gmail.com>
2023-05-24 21:49:32 -0300

    ajustando o trabalho e incluindo capa tirando comentário

--- Segundo_Semestre/Missao3_React_Cadastro_Livros/src/components/capaComponent/capa.tsx
+++ Segundo_Semestre/Missao3_React_Cadastro_Livros/src/components/capaComponent/capa.tsx
@@ -5,7 +5,6 @@ import React from "react";
     <>
     <div className="container  text-center">
     <h1>FACULDADE ESTÁCIO DE SÁ</h1>
-    {/* crie tag imagem */}
     <img className=" w-100" src="https://bancariosjf.com.br/site/fotos/convenio/53/6479486061006685340.jpg" alt="estacio" />
     <h2>Missão Prática - Nível 3</h2>
     <h3>João Luiz Silva Tavares</h3>
