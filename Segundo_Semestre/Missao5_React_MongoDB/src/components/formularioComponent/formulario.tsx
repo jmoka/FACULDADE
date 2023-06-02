26a6b2d785c9ce16cd77e7fbc6b232cfd45d9c1e
jmoka <jmokatavares@gmail.com>
2023-06-02 10:23:21 -0300

    inclusão formulario com alterações

--- Segundo_Semestre/Missao5_React_MongoDB/src/components/formularioComponent/formulario.tsx
+++ Segundo_Semestre/Missao5_React_MongoDB/src/components/formularioComponent/formulario.tsx
@@ -1,124 +1,61 @@
 import React, { useState } from "react";
-import Lista from '../listaFComponent/listaComponenet'
-
-const livrosListagems = [{
-    
-    titulo: "O livro 1",
-    resumo:"resumo teste 1",    
-    editora: "Editora 1",
-    autores: [],
-  },
-  {
-    
-    titulo: "O livro 2",
-    resumo:"resumo teste 2",
-    editora: "Editora 2",
-    autores: [],
-  },
-  {
-    
-    titulo: "O livro 3",  
-    resumo:"resumo teste 3",
-    editora: "Editora 3",
-    autores: [],
-  }
-];
+import { Conectar, FecharConexao } from '../MongoDbComponent/ConexaoMongoDB'
 
 
 
-export default function Formulario() {    
-  const [Listagem, setLivrosListagems] = useState<{
-    titulo: string;
-    resumo:string;    
-    editora: string;
-    autores: [];
-  }[]>(livrosListagems);
+export default function Formulario(){
+  const [titulo, setTitulo] = useState("")
+  const [editora, setEditora] = useState("")
+  const [autores, setAutores] = useState("")
+  const [resumo, setResumo] = useState("")
 
-  const [listaVisivel, setListaVisivel] = useState(false);
-
-  const [novoLivro, setNovoLivro] = useState({
-    titulo: "",
-    resumo:"",
-    editora: "",
-    autores: [],
-  });
-
-  const handleSubmit = (e: { preventDefault: () => void; }) => {
-    e.preventDefault();
   
-    const novoId =
-      Listagem.length > 0 ? Number(Listagem[Listagem.length - 1].id) + 1 : 1;
   
-    setLivrosListagems([...Listagem, { ...novoLivro, id: novoId }]);
-    setNovoLivro({
-      id: "",
-      titulo: "",
-      autor: "",
-      editora: "",
-    });
-  };
-
-  const livrosCadastrados = () => {
-    return Listagem.map((livro) => (
-      <tr key={livro.id}>
-        <th scope="row">{livro.id}</th>
-        <td>{livro.titulo}</td>
-        <td>{livro.editora}</td>
-        <td>{livro.autor}</td>
-      </tr>
-    ));
-  };
-  <Lista livros={livrosCadastrados} />
-
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form>
         <div className="mb-3">
           <label className="form-label">Título</label>
           <input
             type="text"
             className="form-control"
             placeholder="Digite o Título"
-            value={novoLivro.titulo}
-            onChange={(e) =>
-              setNovoLivro({ ...novoLivro, titulo: e.target.value })
-            }
-          />
+            value={titulo}
+            />
         </div>
-        <div className="mb-3">
-          <label className="form-label">Autor</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Digite o Autor"
-            value={novoLivro.autor}
-            onChange={(e) =>
-              setNovoLivro({ ...novoLivro, autor: e.target.value })
-            }
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Editora</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Digite a Editora"
-            value={novoLivro.editora}
-            onChange={(e) =>
-              setNovoLivro({ ...novoLivro, editora: e.target.value })
-            }
-          />
-        </div>
-        <button className="btn btn-primary" type="submit" onClick={() => setListaVisivel(true)} >
-          Adicionar Livro
-        </button>
-       
-        {listaVisivel && <Lista livros={livrosCadastrados} />}
-      </form>
 
-      
+        <div className="form-group">
+    <label className="exampleFormControlSelect1">Editora</label>
+    <select value={editora} className="form-control" id="exampleFormControlSelect1">
+      <option>editora 5</option>
+      <option>editora 1</option>
+      <option>editora 3</option>
+      <option>editora 4</option>
+      <option>editora 5</option>
+      <option>Outra</option>
+    </select>
+  </div>
+
+        <div className="mb-3">
+          <label className="form-label">Autores</label>
+          <input
+          value={autores}
+            type="text"
+            className="form-control"
+            placeholder="Digite a Editora"        
+          />
+        </div>
+
+        <div className="form-group">
+            <label className="exampleFormControlTextarea1">Resumo</label>
+            <textarea value={resumo} className="form-control" id="exampleFormControlTextarea1" ></textarea>
+      </div> 
+       
+      </form>
+    
       
  </>  
  
   );
 }
+  
