6c2c88a0e87bc4b82ec4d260785748dd7767580a
jotaejki <jmokatavares@gmail.com>
2023-09-10 17:32:18 -0300

    trocando o nome da variavel arquivo procedimento 1

--- Terceiro_Semestre/CadastroPOO/src/controle/main_procedimento1.java
+++ Terceiro_Semestre/CadastroPOO/src/controle/main_procedimento1.java
@@ -12,40 +12,40 @@ public class main_procedimento1 {
 		Testes t = new Testes();
 		Gerenciador_Pessoas<PessoaJuridica> pessoaPJ = new Gerenciador_Pessoas<>();
 		Gerenciador_Pessoas<PessoaFisica> pessoaPF = new Gerenciador_Pessoas<>();
-		String nome_arqPF = "dados1.dat";
+		String nome_arqP = "dados1.dat";
 		int pf = 1;
 		int pj = 2;
-		String nome_arqPJ = nome_arqPF; // pode ate achar redundante , mas a ideia e ja deixar pronto para se o caso
+		String nome_arqPJ = nome_arqP; // pode ate achar redundante , mas a ideia e ja deixar pronto para se o caso
 										// quizer criar um rquivo pj separado
 										// eu optei em ter somente 1
 
 		Object jota = new PessoaFisica(pf, 1, "jota", "7678", 23);
 		Object maria = new PessoaFisica(pf, 2, "maria", "987", 23);
-		pessoaPF.carregar(nome_arqPF);
+		pessoaPF.carregar(nome_arqP);
 
 		pessoaPF.ListaPessoas.add((PessoaFisica) jota);
 		pessoaPF.ListaPessoas.add((PessoaFisica) maria);
-		pessoaPF.persistir(nome_arqPF);
+		pessoaPF.persistir(nome_arqP);
 
 		Object magazine_jota = new PessoaJuridica(pj, 3, "magazine_jota", "2345670000198");
 		Object magazine_maria = new PessoaJuridica(pj, 4, "magazine_maria", "67856710000190");
 		pessoaPJ.carregar(nome_arqPJ);
 		pessoaPJ.ListaPessoas.add((PessoaJuridica) magazine_jota);
 		pessoaPJ.ListaPessoas.add((PessoaJuridica) magazine_maria);
-		pessoaPJ.persistir(nome_arqPJ);
+		pessoaPJ.persistir(nome_arqP);
 		
 		
-		pessoaPF.obterTodos_pf_pj(1, nome_arqPF);
-		pessoaPF.obterTodos_pf_pj(2, nome_arqPF);
+		pessoaPF.obterTodos_pf_pj(1, nome_arqP);
+		pessoaPF.obterTodos_pf_pj(2, nome_arqP);
 
 		/*
-		pessoaPF.excluirtodos(nome_arqPF);
-		pessoaPF.excluirtodos(nome_arqPJ);
-		pessoaPJ.persistir(nome_arqPJ);
-		pessoaPJ.persistir(nome_arqPF);
+		pessoaPF.excluirtodos(nome_arqP);
+		pessoaPF.excluirtodos(nome_arqP);
+		pessoaPJ.persistir(nome_arqP);
+		pessoaPJ.persistir(nome_arqP);
 		
-		pessoaPF.obterTodos_pf_pj(1, nome_arqPF);
-		pessoaPF.obterTodos_pf_pj(2, nome_arqPJ);
+		pessoaPF.obterTodos_pf_pj(1, nome_arqP);
+		pessoaPF.obterTodos_pf_pj(2, nome_arqP);
 */
 	};
 }
