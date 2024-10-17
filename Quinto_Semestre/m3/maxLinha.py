c28d18fb4558eb71f2dd2cf5f9931c28dea297f3
jmoka <jmokatavares@gmail.com>
2024-10-17 15:40:28 -0300

    m1, m2, m3

---
+++ Quinto_Semestre/m3/maxLinha.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from ucimlrepo import fetch_ucirepo 
+
+online_retail = fetch_ucirepo(id=352)
+df = online_retail.data.features
+subconjunto = df[['Description', 'Quantity', 'UnitPrice']]
+
+# informa a quantidade de linhas que tem que ser mostrada no console mas não altera a criação do subconjunto
+pd.set_option('display.max_rows', 10) 
+
+print(subconjunto)
+
+# Usando o método head(passando a qquantidade de linhas a serem salvas) no caso as 10 primeiras 
+subConjuntoLinhasEditavel = subconjunto.head(10) 
+
+
+print(subConjuntoLinhasEditavel)
+
+# Salvando o subconjunto em um novo arquivo Excel, to_excelcria um novo arquivo excel e 
+# recebe dois parametros o nome do arquiv.xlsx e o index podenso ser false ou true caso queira que o indice seja arquivado
+subConjuntoLinhasEditavel.to_excel('subconjunto_dados.xlsx', index=False)
+print("Arquivo Excel salvo com sucesso!")
